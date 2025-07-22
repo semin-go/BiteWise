e78c63d73914377aed7cfd570478829aa571590e
newrhyme <ksh000624@naver.com>
2025-07-22 23:22:12 +0900

    feat : 로그인 API

--- bitewise/users/serializers.py
+++ bitewise/users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import User
 
 class UserSignupSerializer(serializers.ModelSerializer) :
@@ -18,3 +19,14 @@ class UserSignupSerializer(serializers.ModelSerializer) :
         user.set_password(password)
         user.save()
         return user
+    
+class UserLoginSerializer(serializers.Serializer) :
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("이메일 또는 비밀번호가 올바르지 않습니다.")
+        data['user'] = user
+        return data
--- bitewise/users/views.py
+++ bitewise/users/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.schemas import openapi
 from drf_yasg.utils import swagger_auto_schema
 from .serializers import UserSignupSerializer
+from .serializers import UserLoginSerializer
 
 class UserSignupView(APIView) :
     @swagger_auto_schema(request_body=UserSignupSerializer)
@@ -12,4 +13,17 @@ class UserSignupView(APIView) :
         if serializer.is_valid():
             serializer.save()
             return Response({'message' : '회원가입 성공!' }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserLoginView(APIView) :
+    @swagger_auto_schema(request_body=UserLoginSerializer)
+    def post(self, request) :
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            return Response({
+                'message' : '로그인 성공!',
+                'email' : user.email,
+                'name' : user.name,
+            },status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
